c25168bb4c280da0e35406c02e11b990d8117d23
Alice Baucke <alicebaucke@me.com>
2017-01-29 16:53:43 -0800

    functions practice

---
+++ functions.py
@@ -0,0 +1,24 @@
+# def my_function(name):
+# 	return "This is " + name + "'s function!!"
+
+# print my_function("Horseshit")
+
+# def hello_print(greeting):
+# 	return greeting
+
+
+# greeting = hello_print("hi")
+# print greeting
+
+
+
+def hello_print(name):
+	return "Hello " + name + "!"
+
+partner_name = "Dino Poop"
+
+print hello_print(partner_name)
+
+
+
+
