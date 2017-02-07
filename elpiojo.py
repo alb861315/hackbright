fb8b4b22b12ba486b4d9cdafeabf1947338e4949
alb861315 <alicebaucke@me.com>
2017-02-06 17:38:10 -0800

    Add files via upload

---
+++ elpiojo.py
@@ -0,0 +1,4 @@
+Dice game played with 1 Dice
+Each player tries to smuggle 100 bottles or more of rum
+The player rolls the dice every time as many times as they like until they decide to pass 
+or get caught by the police (the 1, aka el piojo) losing the all their loot from that turn
